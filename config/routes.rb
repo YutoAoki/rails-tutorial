4661f3d7bb717bdba3718ff02293ee0d37de8ece
YutoAoki <frcsnt@gmail.com>
2018-06-03 21:32:50 +0900

    ７章の終わり

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
 
-
+  resources :users
   # get 'static_pages/help'
   # get 'static_pages/about'
   # get 'static_pages/contact'
