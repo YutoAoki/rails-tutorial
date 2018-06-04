ca5a63d6f4f6e51e698ec528ec5d02b82907b485
YutoAoki <frcsnt@gmail.com>
2018-06-04 23:04:00 +0900

    8章まで終わり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
+
     # debugger
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       flash[:success] = "Welcome to the Sample App!!"
       redirect_to @user
     else
